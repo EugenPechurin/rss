f2bcba42fb92c491ef20313e9f4546d1881f0ba9
EugenPechurin <pecho28@gmail.com>
2017-06-08 17:23:18 +0300

    second commit

---
+++ rss/apps/rss_news/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+
+# Create your views here.
+
+
+def index(request):
+
+    number = random.randrange(0, 100)
+
+    context = {
+        'value': 'Hello Python!!!',
+        'number': str(number),
+    }
+    return  render(request, "index.html", context)
